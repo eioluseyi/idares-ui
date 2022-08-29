074ccfeec2c0753db26df63ebb32a34a578b8b08
emmanuel.imolorhe <imolorhe@gmail.com>
2022-08-29 12:35:36 +0100

    chore: add package.json details chore: add render on index

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 // import reportWebVitals from "./reportWebVitals";
-import Pagination from "./ui-components/pagination/index";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pagination from "./ui-components/pagination";
 export { Pagination };
 // export * from "ui-components/pagination/index";
 // ui-components/pagination";
 
 // reportWebVitals(console.log);
+
+ReactDOM.render(<Pagination />, document.querySelector("#root"));
