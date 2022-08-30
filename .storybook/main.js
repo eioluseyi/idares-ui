f8314b90c059cb3e4b2fe2316f9d988c9bfb0845
emmanuel.imolorhe <imolorhe@gmail.com>
2022-08-30 03:17:57 +0100

    * chore: update package.json * chore: update readme.md * chore: add plugins to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,13 @@ module.exports = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
 		"@storybook/addon-a11y",
+		"@storybook/addon-docs",
 		"@storybook/addon-links",
+		"@storybook/addon-controls",
+		"@storybook/addon-storysource",
+		"@storybook/addon-measure",
+		"@storybook/addon-backgrounds",
+		"@storybook/addon-viewport",
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
 		"@storybook/preset-create-react-app"
